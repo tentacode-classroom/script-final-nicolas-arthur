e88b815a34a6bd07a7a5d9a76f4b69a06c00a003
root <root@vps579026.ovh.net>
2018-12-23 13:10:25 +0100

    update

--- script.sh
+++ script.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-
+clear
 endBook=false
 r=1
 
 function page()
 {
-cat pages/page-$r.txt | grep '\[[$r]]+\]' $r &>/dev/null
+cat pages/page-$r.txt | grep '\[[\+]]+\]' $r &>/dev/null
 RETURN=$?
 if [ $?=0 ]
 then
 	cat pages/page-$r.txt
 	read r
 	clear
+else
+	cat pages/page-$r.txt
+	$endBook=true
 fi
 }
 
